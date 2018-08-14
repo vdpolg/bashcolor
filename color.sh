a13ced1217696b7ecbfba6239beee9ab01a86666
vdpolg <vdpolg@gmail.com>
2018-08-14 20:56:23 +0800

    hello world

---
+++ color.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+export PATH
+color () {
+if [ "$#" -gt 0 ]; then
+
+local code=
+
+case "$1" in
+
+black) code=30 ;;
+
+red) code=31 ;;
+
+green) code=32 ;;
+
+yellow) code=33 ;;
+
+blue) code=34 ;;
+
+magenta) code=35 ;;
+
+cyan) code=36 ;;
+
+white) code=37 ;;
+
+esac
+
+fi
+
+}
+
+echo -e "Hello World!"
+
+exit 0
