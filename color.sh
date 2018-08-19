c069f11ad599674ee88e9e08eac64f7d79336807
vdpolg <vdpolg@gmail.com>
2018-08-19 10:33:57 +0800

    color 易讀性

--- color.sh
+++ color.sh
@@ -4,34 +4,14 @@ function COLS(){
 echo -e "COLOR is \e[${1}m$C" 
 }
 case ${1} in
-"black")
-C=${1};COLS 30
-;;
-"red")
-C=${1};COLS 31
-;;
-"green")
-C=${1};COLS 32
-;;
-"yellow")
-C=${1};COLS 33
-;;
-"blue")
-C=${1};COLS 34
-;;
-"magenta")
-C=${1};COLS 35
-;;
-"cyan")
-C=${1};COLS 36
-;;
-"white")
-C=${1};COLS 37
-;;
-"gray")
-C=${1};COLS 90
-;;
-*)
-echo "Usage ${0} {black|red|green|yellow|blue|magenta|cyan|white|gray}"
-;;
+"black")C=${1};COLS 30;;
+"red")C=${1};COLS 31;;
+"green")C=${1};COLS 32;;
+"yellow")C=${1};COLS 33;;
+"blue")C=${1};COLS 34;;
+"magenta")C=${1};COLS 35;;
+"cyan")C=${1};COLS 36;;
+"white")C=${1};COLS 37;;
+"gray")C=${1};COLS 90;;
+*)echo "Usage ${0} {black|red|green|yellow|blue|magenta|cyan|white|gray}";;
 esac
